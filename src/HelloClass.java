f03ae4b765ba2961746253bd63bcf5c4699e3a7e
Nurzhan <ntolukova@gmail.com>
2020-03-12 19:34:23 -0500

    4th push

--- src/HelloClass.java
+++ src/HelloClass.java
@@ -4,5 +4,7 @@ public class HelloClass {
         System.out.println("Bye!");
         System.out.println("Changes on my tester");
         System.out.println("Changing the master");
+        System.out.println("Changes on my tester");
+        System.out.println("Changes on my tester");
     }
 }
